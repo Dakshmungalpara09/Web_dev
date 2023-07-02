d267fb0cf266b9c68a800f75f797ff5444a3e33c
dakshmungalpara09 <21ce076@charusat.edu.in>
2023-07-02 21:27:44 +0530

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// "use strict";
+// console.log('hello');
+// a='hello world';
+// console.log(a);
+
+// function hello() {
+//     "use strict";
+//     a='hello world';
+// console.log(a);
+// }
+// hello();
+
+function adition(a,b,b){
+    // "use strict";
+    return a+b+b;
+}
+
+console.log(
+    adition(10,20)
+);
---
+++ var_let.js
@@ -0,0 +1,23 @@
+// let x=1;
+
+// if (x===1) {
+//     let x=2;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+
+function hello(){
+    var a=10;
+    function b(){
+        var ba=20;
+        console.log(a);
+        console.log(ba);
+    }
+    b();
+    console.log(a);
+    console.log(ba);
+
+}
+hello();
